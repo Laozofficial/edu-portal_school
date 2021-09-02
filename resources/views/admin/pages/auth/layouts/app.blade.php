b0718cca957b5919c0fba5a5601ca2b638fc55f5
Laozofficial <ellison.bincom@gmail.com>
2021-09-02 12:06:09 +0100

    fixing the login pages

---
+++ resources/views/admin/pages/auth/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="" />
+    <meta name="description" content="Davur - Restaurant Bootstrap Admin Dashboard + FrontEnd" />
+    <meta name="format-detection" content="telephone=no">
+    <title>Easy School - Authentication </title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('admin/images/favicon.png')}}">
+    <link href="{{asset('admin/css/style.css')}}" rel="stylesheet">
+
+</head>
+
+
+<body class="h-100">
+
+    @yield('content')
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{{asset('admin/vendor/global/global.min.js')}}"></script>
+    <script src="{{asset('admin/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
+    <script src="{{asset('admin/js/custom.min.js')}}"></script>
+    <script src="{{asset('admin/js/deznav-init.js')}}"></script>
+
+
+    <!-- layout plugins -->
+    <script src="{{asset('admin/plugins/js/vue.js')}}"></script>
+    <!--importing vue js-->
+</body>
+
+</html>
