05882e5c4b587a16f0138b66984e6e0d88bbbd54
Laozofficial <ellison.bincom@gmail.com>
2021-09-02 11:33:46 +0100

    added the dashboard

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="author" content="Easy Ace" />
+    <meta name="robots" content="" />
+    <meta name="description" content="Easy School Dashboard" />
+    <meta name="format-detection" content="telephone=no">
+    <title>Easy School </title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
+    <link href="{{asset('admin/vendor/jqvmap/css/jqvmap.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('admin/vendor/chartist/css/chartist.min.css')}}">
+    <link href="{{asset('admin/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
+    <link href="{{asset('admin/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('https://cdn.lineicons.com/2.0/LineIcons.css')}}" rel="stylesheet">
+</head>
