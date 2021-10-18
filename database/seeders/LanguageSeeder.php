27bb309485628152b47795e660a496969f971eec
Ifeoluwa Alao <laozofficial@gmail.com>
2021-10-18 16:53:18 +0100

    added seeders

---
+++ database/seeders/LanguageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LanguageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('languages')->insert([
+            'lang_code' => 'ENG',
+            'name' => 'English'
+        ]);
+    }
+}
